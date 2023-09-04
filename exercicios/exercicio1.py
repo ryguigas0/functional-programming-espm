53b55cbe2f8a8df1e93b3f475aa722964ad70a50
Guilherme Oliveira Casagrande <guigacasa@protonmail.ch>
2023-09-04 10:41:43 -0300

    Corrected exercicio1

--- exercicios/exercicio1.py
+++ exercicios/exercicio1.py
@@ -33,40 +33,59 @@ print(f'{x} + {y} = {x+y}')
 
 # 5
 print('#### Exercício 5')
-palavra = 'paralelepípedo'
-output = ''
-
-for c in list(palavra):
-    output += f'{c},'
-
-print(output)
+idade = int(input('Digite a sua idade: '))
+if idade < 18:
+    print('Você é menor de idade')
+elif idade in range(18, 66):
+    print('Você é adulto')
+elif idade in range(66, 101):
+    print('Você está na melhor idade')
+elif idade >= 100:
+    print('Você é centenário')
+else:
+    print('Idade inválida!')
 
 # 6
 print('#### Exercício 6')
+palavra = 'paralelepípedo'
+# output = ''
+
+# for c in list(palavra):
+#     output += f'{c},'
+
+# print(output)
+
+for letra in palavra:
+    print(letra, end=',')
+
+print()
+
+
+# 7
+print('#### Exercício 7')
 nomes = ['Danieli', 'André', 'Marcelo', 'Ana Paula']
 for n in nomes:
     print(n)
 
-# 7
-print('#### Exercício 7')
+# 8
+print('#### Exercício 8')
 from random import randint
 
 n = randint(0, 9)
 
 chute = int(input("Digite um número de 0 a 9: "))
 
-if chute == n:
-    print(f"Número secreto {n} foi encontrado!")
+while chute != n:
+    chute = int(input("Digite um número de 0 a 9: "))
 
-# 8
-print('#### Exercício 8')
-ini = int(input("Digite o indíce de início de sequência de saída: "))
-limit = (ini + 10)
+print(f"Número secreto {n} foi encontrado!")
+
+# 9
+print('#### Exercício 9')
+ini = int(input("Digite o indíce de início de sequência de saída: ")) - 1
 
 fib_list = [0, 1]
-for n in range(1, limit - 1):
+for n in range(1, ini + 10):
     fib_list.append(fib_list[n - 1] + fib_list[n])
 
-print(f'A sua sequência de 10 números depois de {fib_list[ini-1]}: {fib_list[ini:]}')
-
-
+print(f'A sua sequência de 10 números depois de {fib_list[ini]}: {fib_list[ini+1:]}')
