0b7524f61c940b1e02c078c3ebd72d5f71fa8e50
Guilherme Oliveira Casagrande <guigacasa@protonmail.ch>
2023-10-16 14:27:43 -0300

    Fixed fp exercicio4

--- exercicios/fp/exercicio4.py
+++ exercicios/fp/exercicio4.py
@@ -1,6 +1,4 @@
-from functools import reduce
+fib = lambda n, a=1, b=1: [b,0][n>0] or fib(n-1, b, a+b)
 
-def fib(n):
-    return list(reduce(lambda fib_list, x: fib_list + [fib_list[x-1] + fib_list[x-2]], range(2, n), [0, 1]))
-
-print(fib(10))
+for i in range(0, 8):
+    print(fib(i))
